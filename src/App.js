075e0a8859eb7f36e85d21336f8d9b6669258508
Jeremy Mabilangan <jeremy.mabilangan@serino.com>
2024-07-22 21:35:21 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <button class="bg-sky-300 hover:bg-sky-800 text-white px-4 py-2 sm:px-8 sm:py-3">Test</button>
+  )
+}
+
+export default App;
