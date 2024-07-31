88f55ab153d3cf8fa96419ea2931c5eb0af3690a
Jeremy Mabilangan <jeremy.mabilangan@serino.com>
2024-07-31 22:34:53 +0800

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TemperatureChart } from './TemperatureChart';
-export { default as UVIndexChart } from './UVIndexChart';
-export { default as RainChanceChart } from './RainChanceChart';
-export { default as SunChart } from './SunChart';
